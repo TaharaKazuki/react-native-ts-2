7881783836114af824c9ef297a47e5a5a8a6f733
Tahara Kazuki <>
2022-03-26 22:12:08 +0900

    add style shadow

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,9 +1,9 @@
-import { ReactChildren } from 'react'
+import { ReactNode } from 'react'
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 import Colors from '../constant/colors'
 
 type Props = {
-  children: ReactChildren
+  children: ReactNode
   onPress: () => void
 }
 
--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
-import { TextInput, View } from 'react-native'
+import { TextInput, View, StyleSheet } from 'react-native'
+
+import PrimaryButton from '../components/PrimaryButton'
+import Colors from '../constant/colors'
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
-      BUTTONS
+      <PrimaryButton onPress={() => console.info('通過')}>Reset</PrimaryButton>
+      <PrimaryButton onPress={() => console.info('通過')}>
+        Confirm
+      </PrimaryButton>
     </View>
   )
 }
 
 export default StartGameScreen
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary500,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+})
