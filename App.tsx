6815570c90f40f2543865a1ae2f64ee9edcf5836
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-03-26 23:01:17 +0900

    update style

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 import Colors from './constant/colors'
 
 import StartGameScreen from './screens/StartGameScreen'
 
 const App: () => ReactNode = () => {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient
+      colors={[Colors.primary700, Colors.accent500]}
+      style={styles.rootScreen}
+    >
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   )
 }
 
@@ -18,6 +22,5 @@ export default App
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: Colors.accent500,
   },
 })
