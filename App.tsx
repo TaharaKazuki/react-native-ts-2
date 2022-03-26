34e7ab7ca47812e2d77f7099d50fcaa618e8844b
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-03-26 22:57:16 +0900

    update style

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import { ReactNode } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import Colors from './constant/colors'
 
 import StartGameScreen from './screens/StartGameScreen'
 
 const App: () => ReactNode = () => {
-  return <StartGameScreen />
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: Colors.accent500,
+  },
+})
