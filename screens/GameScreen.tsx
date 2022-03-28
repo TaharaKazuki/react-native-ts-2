c66aba014de91eadce925116483988cc52d20451
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-03-28 23:48:11 +0900

    update

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,15 @@
-import { Text } from 'react-native'
+import { View, Text } from 'react-native'
 
 const GameScreen = () => {
-  return <Text>GameScreen</Text>
+  return (
+    <View>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  )
 }
 
 export default GameScreen
