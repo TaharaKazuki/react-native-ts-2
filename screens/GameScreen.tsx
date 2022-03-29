6fe12e1943a775b0e1951b18d0b7f84ebf6d5c52
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-03-29 22:38:36 +0900

    add component

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,28 @@
+import { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import Title from '../components/Title'
 
-const GameScreen = () => {
+const generateRandomBetween = (
+  min: number,
+  max: number,
+  exclude: number
+): number => {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude)
+  } else {
+    return rndNum
+  }
+}
+
+type Props = {
+  userNumber: number
+}
+
+const GameScreen = ({ userNumber }: Props) => {
+  const initialGuess = generateRandomBetween(1, 100, userNumber)
+  const [currentGuess, setCurrentGuess] = useState<number>(initialGuess)
+
   return (
     <View style={styles.screen}>
       <Title>{"Opponent's Guess"}</Title>
